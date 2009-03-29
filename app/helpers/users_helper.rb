f11297e790414ade3f1b198082e45faafbfa6219
Tse-Ching Ho <tsechingho@gmail.com>
2009-03-29 10:18:29 +0800

    adjust helper and views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,4 @@ module UsersHelper
   def authentication_toggle
     link_to_function t('users.authentication_token'), "$('password_box').toggle(); $('openid_box').toggle();"
   end
-  
-  def openid_link
-    link_to t('activerecord.attributes.user.openid_identifier'), "http://openid.net/"
-  end
 end
