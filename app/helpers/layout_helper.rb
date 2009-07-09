1a204b51d1418f8843b69083916e4cc45c10bf92
Tse-Ching Ho <tsechingho@gmail.com>
2009-07-09 14:41:46 +0800

    adjust layout & helper

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -18,6 +18,6 @@ module LayoutHelper
 
   def javascript(*args)
     args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
-    content_for(:head) { javascript_include_tag(*args) }
+    content_for(:foot) { javascript_include_tag(*args) }
   end
 end
